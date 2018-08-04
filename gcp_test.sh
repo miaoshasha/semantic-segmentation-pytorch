6764fb00a044d17b84cc58568683f00b745d1e54
Yisha <yisha@ubiquity6.com>
2018-08-03 18:59:51 -0700

    package complete, job submit ok

--- gcp_test.sh
+++ gcp_test.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 # update gcloud
 gcloud components update
-
+REGION=us-central1
 # set up cloud bucket for source code
 PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 SOURCE_BUCKET_NAME=${PROJECT_ID}-mlengine-pytorch_trial
+gsutil mb -l $REGION gs://$SOURCE_BUCKET_NAME
 
 # package code
 python setup.py sdist
 
 # upload packaged source code
 local_file_path="/Users/yishasun/Documents/dl_models/sem_seg/semantic-segmentation-pytorch/dist"
-file_name="seg_pytorch-0.1.tar.gz"
+file_name="trainer-0.1.tar.gz"
 SOURCE_CODE="gs://$SOURCE_BUCKET_NAME/project/$file_name"
 gsutil cp $local_file_path/$file_name $SOURCE_CODE
 #uploading to cloud to keep track
@@ -19,7 +20,7 @@ gsutil cp $local_file_path/$file_name $SOURCE_CODE
 # train log bucket
 BUCKET_NAME_SUR=${PROJECT_ID}-pytorch_trial
 echo $BUCKET_NAME_SUR
-REGION=us-central1
+
 
 PROJECT="project"
 
@@ -48,29 +49,32 @@ ADE20K="ade20k"     # <<<<<<<<<<<=========change here!!!
 CKPT="ckpt_ade20k"     #<<<<<<<<<<<<<<==========
 
 # custom dependency bucket
-DEPENDENCY_SLIM="/Users/yishasun/Documents/dl_models/models/research/slim/dist/slim-0.1.tar.gz"
+#DEPENDENCY_SLIM="/Users/yishasun/Documents/dl_models/models/research/slim/dist/slim-0.1.tar.gz"
 
 
 #pip install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl
 #pip install torchvision 
 
 # hardware to use
-TIER="BASIC_GPU"
+TIER="BASIC"
 
-MODEL_PATH=./baseline-resnet34_dilated8-psp_bilinear
-RESULT_PATH=./
+MODEL_PATH="./baseline-resnet34_dilated8-psp_bilinear"
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+    --stream-logs \
+    --staging-bucket $PACKAGE_STAGING_BUCKET \
+    --job-dir "${OUTPUT_DIR}"  \
+    --scale-tier "${TIER}"  \
+    --module-name trainer.test \
+    --region "${REGION}" \
+    --packages "${SOURCE_CODE}" \
+    -- \
+    --model_path="${MODEL_PATH}" \
+    --test_img="./davidgarrett.jpg" \
+    --arch_encoder="resnet34_dilated8" \
+    --arch_decoder="psp_bilinear" \
+    --fc_dim=2048 \
+    --result="${OUTPUT_DIR}"
 
 
-python3 -u test.py \
-  --model_path $MODEL_PATH \
-  --test_img ./davidgarrett.jpg \
-  --arch_encoder resnet34_dilated8 \
-  --arch_decoder psp_bilinear \
-  --fc_dim 2048 \
-  --result $RESULT_PATH
 
-deactivate
-
-end_date=`date +"%Y-%m-%d %T.%6N"`
-echo "$end_date #1:Script ends"
-echo "takes $end_date-$start_date"
